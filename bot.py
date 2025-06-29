abd5cc00adb98edb784c52ba4a25b5dea1dff16a
zvezdi1 <lilkenny24@mail.ru>
2025-06-29 15:38:07 +0300

    Add files via upload

--- bot.py
+++ bot.py
@@ -1,18 +1,19 @@
-import os
 from telegram.ext import Updater, CommandHandler
+import logging
 
-TOKEN = os.getenv("TELEGRAM_TOKEN")
+BOT_TOKEN = "7872889474:AAFC7pARBWCDWjXCLLHEja8tCpcHnQxDA2c"
 
-if not TOKEN:
-    raise ValueError("TELEGRAM_TOKEN не установлен в переменных окружения!")
+logging.basicConfig(format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO)
 
 def start(update, context):
-    update.message.reply_text("Привет! Бот запущен.")
+    update.message.reply_text("Бот работает!")
 
 def main():
-    updater = Updater(TOKEN)
-    dp = updater.dispatcher
-    dp.add_handler(CommandHandler("start", start))
+    updater = Updater(token=BOT_TOKEN, use_context=True)
+    dispatcher = updater.dispatcher
+
+    dispatcher.add_handler(CommandHandler("start", start))
+
     updater.start_polling()
     updater.idle()
 
